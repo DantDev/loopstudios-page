b64ba15a34c930055107c7aacf4f6b0b68c2f056
DantDev <118768256+DantDev@users.noreply.github.com>
2024-01-25 22:25:10 +0000

    Initial deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
-import imgHero from "./assets/desktop/image-hero.jpg"
 import logo from "./assets/logo.svg"
+import imgHero from "./assets/desktop/image-hero.jpg"
+import imgInteractive from "./assets/desktop/image-interactive.jpg"
 
 function App() {
 
@@ -11,8 +12,29 @@ function App() {
           <div className="logo-div">
             <img src={logo} />
           </div>
+          <div className="navigation">
+            <input type="checkbox" className="toggle-menu"/>
+            <div className="hamburger"></div>
+            <ul className="menu">
+              <li className="menu-item"><a href="">About</a></li>
+              <li className="menu-item"><a href="">Careers</a></li>
+              <li className="menu-item"><a href="">Events</a></li>
+              <li className="menu-item"><a href="">Product</a></li>
+              <li className="menu-item"><a href="">Support</a></li>
+            </ul>
+          </div>
         </nav>
+        <div className="header-info">
+            <h1 className="header-text">
+              <span>IMMERSIVE</span>
+              <span>EXPERIENCES</span>
+              <span>THAT DELIVER</span>
+            </h1>
+        </div>
         <img className="hero-background" src={imgHero} />
+      </div>
+      <div className="content-1">
+        <img className="img-interactive" src={imgInteractive} />
       </div>
     </div>
   )
