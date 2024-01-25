a3edaf290def20239dca4b092b91e079dd52f845
DantDev <118768256+DantDev@users.noreply.github.com>
2024-01-25 22:50:24 +0000

    text added to content-1

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,10 @@ function App() {
       </div>
       <div className="content-1">
         <img className="img-interactive" src={imgInteractive} />
+        <div className="content-1-info">
+          <h2 className="content-1-title">THE LEADER IN INTERACTIVE VR</h2>
+          <p className="content-1-text">Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
+        </div>
       </div>
     </div>
   )
