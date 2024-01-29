47617380748e06707c09e9af8173e7b2d88fb199
DantDev <118768256+DantDev@users.noreply.github.com>
2024-01-29 20:31:17 +0000

    footer added , navigation hover effects implemented

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ import aboveImg from "./assets/desktop/image-from-above.jpg"
 import borealisImg from "./assets/desktop/image-pocket-borealis.jpg"
 import curiosityImg from "./assets/desktop/image-curiosity.jpg"
 import fisheyeImg from "./assets/desktop/image-fisheye.jpg"
+import facebookLogo from "./assets/icon-facebook.svg"
+import twitterLogo from "./assets/icon-twitter.svg"
+import pinterestLogo from "./assets/icon-pinterest.svg"
+import instagramLogo from "./assets/icon-instagram.svg"
 
 function App() {
 
@@ -24,19 +28,19 @@ function App() {
             <input type="checkbox" className="toggle-menu"/>
             <div className="hamburger"></div>
             <ul className="menu">
-              <li className="menu-item"><a href="">About</a></li>
-              <li className="menu-item"><a href="">Careers</a></li>
-              <li className="menu-item"><a href="">Events</a></li>
-              <li className="menu-item"><a href="">Product</a></li>
-              <li className="menu-item"><a href="">Support</a></li>
+              <li className="menu-item"><a href="">A<span>bou</span>t</a></li>
+              <li className="menu-item"><a href="">Ca<span>ree</span>rs</a></li>
+              <li className="menu-item"><a href="">E<span>ven</span>ts</a></li>
+              <li className="menu-item"><a href="">Pr<span>odu</span>ct</a></li>
+              <li className="menu-item"><a href="">Su<span className="text-under">ppo</span>rt</a></li>
             </ul>
           </div>
         </nav>
         <div className="header-info">
             <h1 className="header-text">
-              <span>IMMERSIVE</span>
-              <span>EXPERIENCES</span>
-              <span>THAT DELIVER</span>
+              <span className="text-block">IMMERSIVE</span>
+              <span className="text-block">EXPERIENCES</span>
+              <span className="text-block">THAT DELIVER</span>
             </h1>
         </div>
         <img className="hero-background" src={imgHero} />
@@ -56,40 +60,60 @@ function App() {
         <div className="content-2-imgs-1">
           <div className="content-2-img">
             <img src={earthImg} />
-            <h3 className="img-title"><span>DEEP</span> EARTH</h3>
+            <h3 className="img-title"><span className="text-block">DEEP</span> EARTH</h3>
           </div>
           <div className="content-2-img">
             <img src={arcadeImg} />
-            <h3 className="img-title"><span>NIGHT</span> ARCADE</h3>
+            <h3 className="img-title"><span className="text-block">NIGHT</span> ARCADE</h3>
           </div>
           <div className="content-2-img">
             <img src={soccerImg} />
-            <h3 className="img-title"><span>SOCCER</span> TEAM VR</h3>
+            <h3 className="img-title"><span className="text-block">SOCCER</span> TEAM VR</h3>
           </div>
           <div className="content-2-img">
             <img src={gridImg} />
-            <h3 className="img-title"><span>THE</span> GRID</h3>
+            <h3 className="img-title"><span className="text-block">THE</span> GRID</h3>
           </div>
         </div>
         <div className="content-2-imgs-2">
           <div className="content-2-img">
             <img src={aboveImg} />
-            <h3 className="img-title"><span>FROM UP</span> ABOVE VR</h3>
+            <h3 className="img-title"><span className="text-block">FROM UP</span> ABOVE VR</h3>
           </div>
           <div className="content-2-img">
             <img src={borealisImg} />
-            <h3 className="img-title"><span>POCKET</span> BOREALIS</h3>
+            <h3 className="img-title"><span className="text-block">POCKET</span> BOREALIS</h3>
           </div>
           <div className="content-2-img">
             <img src={curiosityImg} />
-            <h3 className="img-title"><span>THE</span> CURIOSITY</h3>
+            <h3 className="img-title"><span className="text-block">THE</span> CURIOSITY</h3>
           </div>
           <div className="content-2-img">
             <img src={fisheyeImg} />
-            <h3 className="img-title"><span>MAKE IT</span> FISHEYE</h3>
+            <h3 className="img-title"><span className="text-block">MAKE IT</span> FISHEYE</h3>
           </div>
         </div>
       </div>
+      <footer>
+        <div className="footer-logos">
+          <img className="footer-logo" src={logo} />
+          <div className="social-logos">
+            <img className="logo-single" src={facebookLogo} />
+            <img className="logo-single" src={twitterLogo} />
+            <img className="logo-single" src={pinterestLogo} />
+            <img className="logo-single" src={instagramLogo} />
+          </div>
+        </div>
+        <div className="footer-nav">
+          <p className="footer-menu-item"><a href="">About</a></p>
+          <p className="footer-menu-item"><a href="">Careers</a></p>
+          <p className="footer-menu-item"><a href=""> Events</a></p>
+          <p className="footer-menu-item"><a href="">Products</a></p>
+          <p className="footer-menu-item"><a href="">Support</a></p>
+          <div className="copyright">
+            <p className="copyright-text">© 2021 Loopstudios. All rights reserved.</p></div>
+        </div>
+      </footer>
     </div>
   )
 }
